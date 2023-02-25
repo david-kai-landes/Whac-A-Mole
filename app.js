71d7ad7644231aeab8fa36e006bad5ae3850ea5b
DavidKai-hub <67291503+DavidKai-hub@users.noreply.github.com>
2023-02-24 21:34:09 -0500

    done :( that was quick

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const score = document.querySelector("#score");
 //
 let result = 0;
 let hitPosition;
+let currentTime = 10;
+let timerId = null;
 //
 function randomSquare() {
   squares.forEach((square) => {
@@ -17,7 +19,6 @@ function randomSquare() {
   hitPosition = randomSquare.id;
 }
 //
-//
 squares.forEach((square) => {
   square.addEventListener("mousedown", () => {
     if (square.id == hitPosition) {
@@ -29,9 +30,20 @@ squares.forEach((square) => {
 });
 //
 function moveMole() {
-  let timerId = null;
   timerId = setInterval(randomSquare, 1000);
 }
 //
-
 moveMole();
+//
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert("GAME OVER MDFKA!! YOUR SCORE " + result);
+  }
+}
+//
+let countDownTimerId = setInterval(countDown, 1000);
